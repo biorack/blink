0c6143bfcdc2a351252800869f546cad2a2d1d8b
tharwood3 <tharwood@lbl.gov>
2023-07-07 10:03:06 -0700

    feat: scoring gpu acceleration

--- blink/blink.py
+++ blink/blink.py
@@ -103,13 +103,13 @@ def discretize_spectra(mzis_s1: list, mzis_s2: list, precursor_mzs_s1: list, pre
 
     return discretized_spectra
 
-def score_sparse_spectra(discretized_spectra: dict) -> dict:
+def score_sparse_spectra(discretized_spectra: dict, gpu: bool = False) -> dict:
     """Generates scores and matching ion counts for discretized spectra"""
     
     if 'mdi' and 'mdc' in discretized_spectra['s1']:
-        scores = _score_mass_diffs(discretized_spectra) 
+        scores = _score_mass_diffs(discretized_spectra, gpu=gpu) 
     else:
-        scores = _score_sparse_matrices(discretized_spectra)
+        scores = _score_sparse_matrices(discretized_spectra, gpu=gpu)
         
     return scores
 
